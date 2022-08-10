4aee2c6881d0f33bd4d1fa6be710b86cd1350b3b
NVMate <denys.miand@nure.ua>
2022-08-10 03:47:52 +0300

    Clean initially created create-react-app code Add functions to get and reset race data.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
 
-function App() {
+export default function App() {
+  const [socket, setSocket] = useState(null);
+  const [raceProgress, setRaceProgress] = useState(null);
+
+  const startRace = () => {
+    socket.emit("start");
+  };
+  const stopTimer = () => {
+    socket.disconnect();
+    socket.connect("http://localhost:3002");
+  };
+
+  const getRace = () => {
+    socket.on("ticker", (response) => setRaceProgress(response));
+  };
+
+  function resetRace() {
+    stopTimer();
+    startRace();
+    getRace();
+  }
+
+  useEffect(() => {
+    setSocket(io.connect("http://localhost:3002"));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React.
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={resetRace} />
+      {raceProgress ? (
+        <>
+          <ul>
+            {raceProgress.map((horse) => (
+              <li key={horse.name}>
+                <p>{horse.name}</p>
+                <p>{horse.distance}</p>
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <h1>nothing to show </h1>
+      )}
+    </>
   );
 }
-
-export default App;
