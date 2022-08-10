28428475aa290d8b98c00df6c7d42d9daec9d0b1
NVMate <denys.miand@nure.ua>
2022-08-10 19:50:59 +0300

    Add sorting of the received horse results

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import io from "socket.io-client";
 
 export default function App() {
@@ -15,7 +15,46 @@ export default function App() {
   };
 
   const getRace = () => {
-    socket.on("ticker", (response) => setRaceProgress(response));
+    let newRace = [];
+    let placement = 1;
+
+    function horseIsUndefined(horse) {
+      if (newRace.length > 0) {
+        return (
+          newRace.find((newHorse) => horse.name === newHorse.name).placement ===
+          undefined
+        );
+      }
+      return false;
+    }
+
+    function horseHasPlacement(horse) {
+      let toCheck = newRace.find((newHorse) => horse.name === newHorse.name);
+      if (toCheck !== undefined) {
+        if (toCheck.placement) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    socket.on("ticker", (response) => {
+      newRace = response.map((horse) => {
+        if (horseHasPlacement(horse)) {
+          return newRace.find((newHorse) => horse.name === newHorse.name);
+        }
+        if (horseIsUndefined(horse) && horse.distance >= 1000) {
+          horse = { ...horse, placement: placement };
+          placement++;
+        }
+        return horse;
+      });
+      setRaceProgress(
+        newRace.sort(
+          (a, b) => b.distance - a.distance || a.placement - b.placement
+        )
+      );
+    });
   };
 
   function resetRace() {
@@ -29,22 +68,25 @@ export default function App() {
   }, []);
 
   return (
-    <>
-      <button onClick={resetRace} />
-      {raceProgress ? (
-        <>
-          <ul>
+    <div className="racePage">
+      <div className="racePageContent">
+        {raceProgress ? (
+          <ul className="raceProgress">
             {raceProgress.map((horse) => (
               <li key={horse.name}>
-                <p>{horse.name}</p>
+                <p>
+                  {horse.placement && <span>{horse.placement + " "}</span>}
+                  {horse.name}
+                </p>
                 <p>{horse.distance}</p>
               </li>
             ))}
           </ul>
-        </>
-      ) : (
-        <h1>nothing to show </h1>
-      )}
-    </>
+        ) : (
+          <h1>nothing to show </h1>
+        )}
+        <button onClick={resetRace} />
+      </div>
+    </div>
   );
 }
